0bfd37c15cbc6aea098ba18b0d7642312c66d61d
Bogdan <ibogdanpopov@gmail.com>
2018-05-27 18:04:33 +0300

    Link for user profile in user statistic page was changed

--- src/main/java/ru/kpfu/itis/group11501/smartmuseum/model/enums/EntityName.java
+++ src/main/java/ru/kpfu/itis/group11501/smartmuseum/model/enums/EntityName.java
@@ -9,7 +9,7 @@ public enum EntityName {
     PROJECTORSVIDEOS("ПроеткорВидео",  null),  //not page for individual projector_video; only add
     VIDEO("Видео", null),   //not page for individual video
     PROJECTOR("Проектор", "/projector/"),
-    USER("Пользователь", "/user/");
+    USER("Пользователь", "/admin/users/");
     private final String rusName;
     private final String link;
 
